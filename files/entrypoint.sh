f4d1eff03c6d5f019d0d85c65e7fc86deb55c3a1
madebytimo <t.schnaible@madebytimo.de>
2025-07-13 11:02:10 +0200

    fix crontab creation

--- files/entrypoint.sh
+++ files/entrypoint.sh
@@ -3,9 +3,11 @@ set -e -o pipefail
 
 mkdir --parents /media/compatible-media-creator/{input,output}
 
-echo "$CRON root bash --login -c 'ENCODER_CPU=$ENCODER_CPU \
-    NICENESS_ADJUSTMENT=$NICENESS_ADJUSTMENT SCHED_POLICY=$SCHED_POLICY \
-    run.sh > /proc/1/fd/1 2>&1'" \
-    > /media/cron/compatible-media-creator
+if [[ -n "$CRON_SCHEDULE" ]]; then
+    echo "$CRON_SCHEDULE root bash --login -c 'ENCODER_CPU=$ENCODER_CPU \
+        NICENESS_ADJUSTMENT=$NICENESS_ADJUSTMENT SCHED_POLICY=$SCHED_POLICY \
+        run.sh > /proc/1/fd/1 2>&1'" \
+        > /media/cron/compatible-media-creator
+fi
 
 exec /entrypoint-cron.sh "$@"
